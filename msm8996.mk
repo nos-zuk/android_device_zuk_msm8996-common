3c906c271c9997642dc164fafd0e0e9b7f20a000
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-27 19:41:04 -0700

    msm8996: Compile libandroid_net for IMS
    Change-Id: I4a3687a1639fe37b9652d651b9be773a09f63e14
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -211,6 +211,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Net
+PRODUCT_PACKAGES += \
+    libandroid_net
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
